e936574e8fccee5b7af763adcabfed050abf80c0
Ahmad Murad <ahmadmurad@Ahmads-MBP.broadband>
2020-05-31 21:05:10 +0100

    adds RGB sliders, colour labels, and colour name label. Set colour, re-set and info buttons. RGB & HSB  segmented control to the storyboard.

---
+++ Week01/Colour Picker/Colour Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colour Picker
+//
+//  Created by Ahmad Murad on 31/05/2020.
+//  Copyright © 2020 Ahmad Murad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
